6eb1919bc42afde6cc530556f11415792e2e59d5
Lealx7 <167640978+Lealx7@users.noreply.github.com>
2026-03-20 09:26:49 -0300

    Calculadora if/else
    Cria lógica inicial da calculadora em C# (Console), com fluxo baseado em estruturas condicionais IF/ELSE para processamento das operações matemáticas.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Operações: \nSoma(+) \nSubstração (-)  \nMultiplicação (*) \nDivisão (/)\n");
+
+        Console.Write("Digite o Primeiro Numero: ");
+        double n1 = double.Parse(Console.ReadLine());
+    
+        Console.Write("Digite o Segundo Numero: ");
+        double n2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a Operação: ");
+        string operacao = Console.ReadLine();
+
+        double resultado = 0;
+        bool operacaoValida  = true; 
+        
+        if(operacao == "+")
+        {
+            resultado = n1 + n2;
+        }
+
+        else if(operacao == "-")
+        {
+            resultado = n1 -n2;
+        }
+
+        else if(operacao == "*")
+        {
+            resultado = n1 * n2;
+        }
+
+        else if(operacao == "/")
+        {
+            if(n2 == 0)
+            {
+                Console.WriteLine("Erro: Divisão Por zero.");
+                operacaoValida = false;
+            }
+
+            else
+            {
+                resultado = n1 / n2;
+            }
+        }
+
+        else
+        {
+            Console.WriteLine("Operação Invalida.");    
+        }
+
+        if (operacaoValida)
+        {
+            Console.WriteLine($"Resultado: {resultado}");
+        }
+
+
+
+    }
+}
